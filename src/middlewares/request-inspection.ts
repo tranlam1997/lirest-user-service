27ee56d9ab5916930913b0bd18e3a2232ef2b068
Tran Tuan Lam <foet1997@gmail.com>
2023-01-15 22:30:53 +0700

    Add request inspection middleware

---
+++ src/middlewares/request-inspection.ts
@@ -0,0 +1,26 @@
+import {logger} from '../common/winston';
+import { BaseRequest } from '../base/request';
+import { NextFunction } from 'express';
+import { BaseResponse } from '@src/base/response';
+import requestIp from 'request-ip';
+
+const RequestLogger = logger('RequestInspectionMiddleware');
+// log essentail info about the request
+export default function requestInspectionMiddleware(req: BaseRequest, _res: BaseResponse, next: NextFunction) {
+  RequestLogger.info(`Request received:
+    Method: ${req.method},
+    Path: ${req.path},
+    Body: ${JSON.stringify(req.body)},
+    Query: ${JSON.stringify(req.query)},
+    Params: ${JSON.stringify(req.params)},
+    Headers: ${JSON.stringify(req.headers)},
+    Referer: ${req.headers.referer},
+    User-Agent: ${req.headers['user-agent']},
+    IpAddress: ${requestIp.getClientIp(req)},
+    Port: ${req.socket.localPort},
+    OriginalUrl: ${req.originalUrl},
+    Protocol: ${req.protocol},
+    Hostname: ${req.hostname},
+  `);
+  next();
+}
