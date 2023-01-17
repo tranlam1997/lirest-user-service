ef7bc8ea18343345ea1107c82f2f2c0571705c0f
Tran Tuan Lam <foet1997@gmail.com>
2023-01-18 01:48:58 +0700

    Refactor base repository to use class type

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
       node: true,
       jest: true,
     },
+    ignorePatterns: ['*.d.ts'],
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
--- src/base/repository.ts
+++ src/base/repository.ts
@@ -1,54 +1,59 @@
-import { dataSource } from '@src/common/typeorm';
+import { dataSource } from '@src/common/typeorm.config';
 import { EntityTarget, FindManyOptions, FindOneOptions, ObjectLiteral } from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
-export function BaseRepository<T extends ObjectLiteral>(entityTarget: EntityTarget<T>) {
-  return {
-    create(payload: any) {
-      const entity = dataSource.manager.create<T>(entityTarget, payload);
-      return dataSource.manager.save<T>(entity);
-    },
+export class BaseRepository<T extends ObjectLiteral> {
+  private entityTarget: EntityTarget<T>;
 
-    createQueryBuilder(alias: string) {
-      return dataSource.manager.createQueryBuilder<T>(entityTarget, alias);
-    },
+  constructor(entityTarget: EntityTarget<T>) {
+    this.entityTarget = entityTarget;
+  }
 
-    find(options: any, relations?: string[], skip?: number, take?: number) {
-      return dataSource.manager.find<T>(entityTarget, {
-        ...options,
-        relations,
-        skip,
-        take,
-      } as FindManyOptions);
-    },
+  public async create(payload: DeepPartial<T>) {
+    const entity = dataSource.manager.create<T>(this.entityTarget, payload);
+    return dataSource.manager.save<T>(entity);
+  }
 
-    findOne(options: any, relations?: string[]) {
-      return dataSource.manager.findOne<T>(entityTarget, {
-        ...options,
-        relations,
-      } as FindOneOptions);
-    },
+  public async createQueryBuilder(alias: string) {
+    return dataSource.manager.createQueryBuilder<T>(this.entityTarget, alias);
+  }
 
-    findById(id: any, relations?: string[]) {
-      return dataSource.manager.findOne<T>(entityTarget, {
-        where: { id },
-        relations,
-      } as FindOneOptions);
-    },
+  public async find(options: FindManyOptions<T>, relations?: string[], skip?: number, take?: number) {
+    return dataSource.manager.find<T>(this.entityTarget, {
+      ...options,
+      relations,
+      skip,
+      take,
+    });
+  }
 
-    update(conditions: any, payload: any) {
-      return dataSource.manager.update<T>(entityTarget, conditions, payload);
-    },
+  public async findOne(options: FindOneOptions<T>, relations?: string[]) {
+    return dataSource.manager.findOne<T>(this.entityTarget, {
+      ...options,
+      relations,
+    } as FindOneOptions);
+  }
 
-    delete(conditions: any) {
-      return dataSource.manager.delete<T>(entityTarget, conditions);
-    },
+  public async findById(id: string | number, relations?: string[]) {
+    return dataSource.manager.findOne<T>(this.entityTarget, {
+      where: { id },
+      relations,
+    } as FindOneOptions);
+  }
 
-    deleteById(id: any) {
-      return dataSource.manager.delete<T>(entityTarget, id);
-    },
+  public async update(conditions: any, payload: QueryDeepPartialEntity<T>) {
+    return dataSource.manager.update<T>(this.entityTarget, conditions, payload);
+  }
 
-    getEntityRepository() {
-      return dataSource.getRepository<T>(entityTarget);
-    },
-  };
+  public async delete(conditions: any) {
+    return dataSource.manager.delete<T>(this.entityTarget, conditions);
+  }
+
+  public async deleteById(id: string | number) {
+    return dataSource.manager.delete<T>(this.entityTarget, id);
+  }
+
+  public async getEntityRepository() {
+    return dataSource.getRepository<T>(this.entityTarget);
+  }
 }
