ca9155226974f8d974abe9ade3c61ad9556f08da
Tran Tuan Lam <foet1997@gmail.com>
2023-05-05 06:55:14 +0700

    add cors middleware

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,15 @@
 import { Application, Router } from 'express';
 import UsersController from '../modules/users/users.controller';
 import config from 'config';
+import cors from 'cors';
 
 const router = Router();
 const baseUrl = config.get<string>('service.baseUrl');
 
 export default (app: Application) => {
+  app.use(cors({
+    origin: '*',
+  }))
   UsersController(router);
   // test server connection
   app.get(`${baseUrl}/ping`, (_req, res) => {
